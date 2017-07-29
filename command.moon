eb9f26ec408e2551c66616f9398c579df6afbffb
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-07-28 22:42:35 -0700

    almost logging in from commandline

--- command.moon
+++ command.moon
@@ -1,18 +1,27 @@
 lapis = require "lapis"
-csrf = require "lapis.csrf"
+
+import respond_to, json_params from require "lapis.application"
 
 Users = require "models.Users"
 
 class extends lapis.Application
   @path: "/command"
 
-  [command: ""]: =>
-    if @params.command == "help"
-      return layout: false, "Help text would go here, if I was a better programmer."
+  [command: ""]: respond_to {
+    GET: =>
+      return layout: false, status: 405, "Method not allowed."
 
-    elseif @session.id
-      @user = Users\find id: @session.id
-      return layout: false, "This is a work-in-progress. Nothing happens yet."
+    POST: json_params =>
+      if @params.command == "help"
+        return layout: false, "Help text would go here, if I was a better programmer."
 
-    else
-      return layout: false, "You must log in first."
+      elseif @params.command == "login"
+        return layout: false, "Here goes nothing! No really, that doesn't work yet."
+
+      elseif @session.id
+        @user = Users\find id: @session.id
+        return layout: false, "This is a work-in-progress. Nothing happens yet."
+
+      else
+        return layout: false, "You must log in first."
+  }
--- static/js/game.js
+++ static/js/game.js
@@ -1,10 +1,43 @@
-var Terminal;
 var commandUrl = "https://ld39.guard13007.com/command";
+
+var Terminal;
+var History;
 
 $(function() {
   $('#terminal').terminal(function(command, term) {
-    if (command.indexOf("exit ") == 0) {
+    var args = command.split(" ");
+
+    if (args[0] == "exit") {
+      // TODO have this log you out
       return false
+
+    } else if (args[0] == "login") {
+      var user, password
+      History.disable();
+      if (args[1]) {
+        user = args[1]
+      } else {
+        Terminal.push(function(c) {
+          user = c;
+          Terminal.pop();
+        }, {
+          prompt: "Username: "
+        });
+      }
+
+      Terminal.set_mask(true);
+      Terminal.push(function(c) {
+        password = c;
+        Terminal.pop();
+        Terminal.set_mask(false);
+        History.enable();
+      }, {
+        prompt: "Password: "
+      });
+
+      // now log in
+      return $.post(commandUrl, {command: "login", name: user, password: password});
+
     } else {
       // return $.post(commandUrl, {command: command});
       term.pause();
@@ -22,6 +55,7 @@ $(function() {
     historySize: false,
     onInit: function(term) {
       Terminal = term;
+      History = term.history();
     }
   });
 });
