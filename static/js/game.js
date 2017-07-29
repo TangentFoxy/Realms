adffe2a3d01f0cdfa06aae10a03ba0068aae01ab
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-07-28 21:12:30 -0700

    I bit my tonque

--- static/js/game.js
+++ static/js/game.js
@@ -1,7 +1,8 @@
 $(function() {
   $('#terminal').terminal(function(command, term) {
-    return $.post('https://ld39.guard13007.com/command', {command: command});
+    return " " + $.post('https://ld39.guard13007.com/command', {command: command});
   }, {
+    //prompt: ">",
     greetings: "Test?",
     onBlur: function() {
       return false;
