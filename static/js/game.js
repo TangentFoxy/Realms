10382a7d4fba769482992d5c73e4941515d875c8
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-08-01 14:42:44 -0700

    JS isweird

--- static/js/game.js
+++ static/js/game.js
@@ -22,7 +22,7 @@ function update() {
 
       if (version == 0) {
         version = data.version;
-        updateTimer = setTimeout(update, 1);
+        updateTimer = setTimeout(update, 1000);
         return true;
       }
 
